2d5be0ad555b18b715c0dbb17f6bb208d258cc42
Toby <tobyreed0@gmail.com>
2015-12-04 06:44:19 +0000

    Started work on random enemy generation

---
+++ Assets/Scripts/Util/GenerateEnemies.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateEnemies : MonoBehaviour
+{
+
+    public Transform enemyPrefab;
+    public float generateRate = 0.25f;
+
+    private float enemyCooldown;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        enemyCooldown = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyCooldown > 0)
+        {
+            enemyCooldown -= Time.deltaTime;
+        }
+    }
+
+    void createNewEnemy()
+    {
+        enemyCooldown = generateRate;
+
+        // Create a new shot
+        var enemyCreate = Instantiate(enemyPrefab) as Transform;
+
+        // Assign position
+        enemyCreate.position = transform.position;
+    }
+
+    public bool CanAttack
+    {
+        get
+        {
+            return enemyCooldown <= 0f;
+        }
+    }
+}
+
