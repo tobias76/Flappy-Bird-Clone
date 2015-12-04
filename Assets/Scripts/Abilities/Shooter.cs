ac1343d2a582face2ee9fc42db730e72c9b27fd3
Toby <tobyreed0@gmail.com>
2015-12-04 03:20:14 +0000

    Added a shot script
    This is the foundations of allowing things to shoot.

---
+++ Assets/Scripts/Abilities/Shooter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This class is where I keep all generic behaivour to do with shooting
+/// </summary>
+public class Shooter : MonoBehaviour
+{
+
+    /// <summary>
+    /// Variable declaration
+    /// </summary>
+
+    public int shotDamage = 1;
+    public bool isEnemyShot = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // This section destroys shots after a 20 seconds to avoid a memory leak
+        Destroy(gameObject, 20);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
