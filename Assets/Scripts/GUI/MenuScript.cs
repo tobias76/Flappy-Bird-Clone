f244c57862ea20e0442d9cd30b6e24bbee9d130e
Toby <tobyreed0@gmail.com>
2015-12-04 06:04:28 +0000

    First attempt at a main menu
    Here is a functional yet very aesthetically disgusting main menu.

---
+++ Assets/Scripts/GUI/MenuScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    public Canvas quitMenu;
+    public Button startText;
+    public Button exitText;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+
+        //Makes menu invisible on start
+        quitMenu.enabled = false;
+    }
+
+    public void ExitPress()
+    {
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    public void noPress()
+    {
+        quitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void startLevel()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+	
+	}
+}
